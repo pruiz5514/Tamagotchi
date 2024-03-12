7a6681aa9c50a711d9a2afea6a475334ede0967e
pruiz5514 <pablo_ruiza@hotmail.com>
2024-03-11 19:22:23 -0700

    Se agrega archivo de main y se hace funcional las barras y los botones

---
+++ main.js
@@ -0,0 +1,59 @@
+const indicardorSalud = document.querySelector(".healthLevel__bar");
+const botonMedicina = document.querySelector(".medicine-button");
+const indicadorCansancio = document.querySelector(".energyLevel__bar");
+const botonDormir = document.querySelector(".sleep-button");
+
+
+function crearTemporizador(indicador, tiempoInicial) {
+    let tiempo = tiempoInicial;
+    let intervalo;
+
+    function nivelBarras() {
+        indicador.style.width = tiempo + "%";
+    
+        if (tiempo > 66.66) {
+            indicador.style.backgroundColor = "green";
+        } else if (tiempo <= 66.66 && tiempo > 33.33) {
+            indicador.style.backgroundColor = "yellow";
+        } else {
+            indicador.style.backgroundColor = "red";
+        }
+    }
+
+    function iniciar(s) {
+        if (intervalo){
+            clearInterval(intervalo);
+        }
+
+        intervalo = setInterval(() => {
+            if (tiempo > 0) {
+                tiempo--;
+                nivelBarras();
+            } else {
+                console.log("Perdiste");
+            }
+        }, s);
+    }
+
+    function reiniciar(s) {
+        tiempo = tiempoInicial;
+        nivelBarras(); 
+        iniciar(s); 
+    }
+
+    nivelBarras();
+
+    return { iniciar, reiniciar };
+}
+
+
+const temporizadorSalud = crearTemporizador(indicardorSalud, 100);
+const temporizadorCansancio = crearTemporizador(indicadorCansancio, 100);
+
+
+temporizadorSalud.iniciar(200);
+temporizadorCansancio.iniciar(1000);
+
+
+botonMedicina.addEventListener("click", ()=>{temporizadorSalud.reiniciar(200)});
+botonDormir.addEventListener("click", ()=>{temporizadorCansancio.reiniciar(1000)});
